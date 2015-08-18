816f8fb3315046c6650d2b011dc723f91d3b3c61
mindwing <mindwing@gmail.com>
2015-08-18 17:07:11 +0900

    Oddity.go is ready

--- 2-expressive-puzzlers/puzzle-1/Oddity.go
+++ 2-expressive-puzzlers/puzzle-1/Oddity.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func isOdd(i int) bool {
+    return i % 2 == 1
+}
+
 func main() {
-	fmt.Println("안녕, World!")
+	fmt.Println(isOdd(2))
+	fmt.Println(isOdd(1))
+	fmt.Println(isOdd(0))
+	fmt.Println(isOdd(-1))
+	fmt.Println(isOdd(-2))
 }
